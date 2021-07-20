d1df9756eff8d1d67e9bbe7fcabcfb165a23b51e
Reckordp <rmuhammad134@gmail.com>
2021-07-20 08:21:38 +0700

    Add link to style.css

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,6 +3,7 @@
 <head>
 	<title>Control Project</title>
 	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
 </head>
 <body>
 	<h1 align="center" class="fw-bold">Control Project</h1>
